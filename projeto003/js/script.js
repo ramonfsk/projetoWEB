85d9917959adb3d3e01af41557b3fd032d5d2e15
Ramon Ferreira <ramonfsk@gmail.com>
2020-03-02 12:01:53 -0300

    projeto04 - Criando tabelas...

--- projeto003/js/script.js
+++ projeto003/js/script.js
@@ -5,7 +5,8 @@ function selecionarMaterias() {
     checkboxs.forEach(checkbox => {
         if(checkbox.checked == true) {
             //alert(`${checkbox.id}`);
-            divConteudo.innerHTML += checkbox.id;
+            //divConteudo.innerHTML += checkbox.id;
+            divConteudo.innerHTML += `<p>${checkbox.id}</p>`;
         }
     });
 }
